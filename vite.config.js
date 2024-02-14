c94a8dbd2f0ad725dab6227a601b9f5a3db28663
Dheeraj Kumar Sah <86295102+dheeraj20-bot@users.noreply.github.com>
2024-02-14 14:44:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      "/api":"https://serverpro-bv3l.onrender.com"
+      "/api":"https://serverpro-bv3l.onrender.com",
+      changeOrigin: true,
     }
   }
   ,
